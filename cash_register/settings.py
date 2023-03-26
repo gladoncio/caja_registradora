10cee21f57c27a401f860689ee24ca559f1facda
gladoncio <muertedark2@gmail.com>
2023-03-26 02:37:00 -0300

    base de datos etc

--- cash_register/settings.py
+++ cash_register/settings.py
@@ -75,11 +75,15 @@ WSGI_APPLICATION = 'cash_register.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'caja',
+            'USER': 'root',
+            'PASSWORD': '2910',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
     }
-}
 
 
 # Password validation
@@ -124,3 +128,4 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL="core.Usuario"
--- core/models.py
+++ core/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class Usuario(AbstractUser):
+    permisos = models.CharField(max_length=40,null=True, verbose_name="Permiso",default="1")
+    rut = models.CharField(max_length=40,null=True, verbose_name="Rut",default="")
+
