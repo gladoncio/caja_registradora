6a450ce0bc7a1efe429eaefc45f843c4dd906109
gladoncio <muertedark2@gmail.com>
2023-03-25 23:34:29 -0300

    proyecto

---
+++ core/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ModelForm, fields, Form
+from django.contrib.auth.forms import AuthenticationForm
+from django.forms.widgets import PasswordInput, TextInput
+
+
+
