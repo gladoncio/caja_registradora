e7515a9111712fe7139002b97821501aec5edf69
gladoncio <42326122+gladoncio@users.noreply.github.com>
2023-09-08 04:42:41 -0300

    actualizacion

--- caja_registradora-cash2/api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ProductoSearchAPIView
-
-app_name = 'api'
-
-urlpatterns = [
-    path('busqueda/', ProductoSearchAPIView.as_view(), name='producto-search'),
-]
