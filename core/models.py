85689418fcbd6fc3213f865c6c471bb103b239d9
gladoncio <42326122+gladoncio@users.noreply.github.com>
2023-10-16 22:33:28 -0300

    imagen default update

--- core/models.py
+++ core/models.py
@@ -60,7 +60,7 @@ class Producto(models.Model):
     precio = models.DecimalField(max_digits=10, decimal_places=2)
     codigo_barras = models.CharField(max_length=20, unique=True, blank=True, null=True)
     gramaje = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)
-    foto = models.ImageField(upload_to='productos/', null=True, blank=True)
+    foto = models.ImageField(upload_to='productos/', null=True, blank=True, default='productos/default.jpg')
     descripcion = models.TextField(blank=True, null=True)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE, related_name='productos_departamento', blank=True, null=True)
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE, blank=True, null=True)
