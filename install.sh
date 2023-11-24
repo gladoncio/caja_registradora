f07ea3efb7d1128b818ea8390e370a3aa55c35a3
gladoncio <42326122+gladoncio@users.noreply.github.com>
2023-11-23 23:01:37 -0600

    install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+
+sudo apt update
+sudo apt upgrade -y
+
+# Instalar Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Verificar la instalación de Docker Compose
+docker-compose --version
