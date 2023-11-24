dae5f02359850e714dc3bcd2c0e822a38e9c7672
gladoncio <42326122+gladoncio@users.noreply.github.com>
2023-11-23 23:11:56 -0600

    install

--- install.sh
+++ install.sh
@@ -1,13 +1,23 @@
+#!/bin/bash
 
+# Actualizar los paquetes del sistema
 sudo apt update
 sudo apt upgrade -y
+
+# Instalar Docker
+sudo apt install -y docker.io
+
+# Iniciar y habilitar el servicio Docker
+sudo systemctl start docker
+sudo systemctl enable docker
 
 # Instalar Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-# Verificar la instalación de Docker Compose
+# Verificar la instalación de Docker y Docker Compose
+docker --version
 docker-compose --version
 
+# Construir imágenes con Docker Compose (reemplaza esto con tus propios comandos de Docker Compose)
 docker-compose build
-
