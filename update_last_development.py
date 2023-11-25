8cbe683a93e0f6e88f7a918f99c445aca11d9cdc
gladoncio <42326122+gladoncio@users.noreply.github.com>
2023-11-25 01:02:59 -0300

    update

--- update_last_development.py
+++ update_last_development.py
@@ -3,8 +3,6 @@ from datetime import datetime
 import os
 import subprocess
 
-latest_release_name = "v1.3.1"  # Reemplaza esto con el nombre de tu última versión
-
 try:
     # Guarda los cambios locales antes de hacer checkout
     subprocess.run(["git", "stash"], check=True)
@@ -15,7 +13,7 @@ try:
     # Obtiene los últimos cambios de la rama principal
     subprocess.run(["git", "pull", "origin", "main"], check=True)
 
-    message = f"Checkout exitoso a la última update ({latest_release_name})."
+    message = f"volviendo a la ultima update."
 
 except subprocess.CalledProcessError as e:
     message = f"Error al hacer checkout: {e}"
