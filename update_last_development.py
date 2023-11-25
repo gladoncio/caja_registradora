11f6a914b4560fd6486d070714a3b1572b915dd2
gladoncio <42326122+gladoncio@users.noreply.github.com>
2023-11-25 01:06:12 -0300

    update

--- update_last_development.py
+++ update_last_development.py
@@ -4,10 +4,7 @@ import os
 import subprocess
 
 try:
-    # Guarda los cambios locales antes de hacer checkout
-    subprocess.run(["git", "stash"], check=True)
 
-    # Vuelve a la rama principal
     subprocess.run(["git", "checkout", "main"], check=True)
 
     # Obtiene los últimos cambios de la rama principal
@@ -18,5 +15,4 @@ try:
 except subprocess.CalledProcessError as e:
     message = f"Error al hacer checkout: {e}"
 
-# Restaura los cambios locales después de hacer checkout
-subprocess.run(["git", "stash", "apply"], check=True)
+
