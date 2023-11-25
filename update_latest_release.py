39e6f471a59aecb7c00167dbac79814136d66995
gladoncio <42326122+gladoncio@users.noreply.github.com>
2023-11-25 00:25:28 -0300

    update python script

---
+++ update_latest_release.py
@@ -0,0 +1,44 @@
+import requests
+from datetime import datetime
+import os
+import subprocess
+
+
+
+def check_github_version():
+    url = "https://api.github.com/repos/gladoncio/caja_registradora/releases/latest"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        latest_version = data["tag_name"]
+        return latest_version
+    else:
+        # Manejar el error, puedes imprimir un mensaje o levantar una excepción según tus necesidades
+        return None
+    
+latest_release_name = check_github_version()
+
+if latest_release_name is not None:
+    try:
+        # Obtiene el directorio actual antes de cambiarlo
+        current_directory = os.getcwd()
+
+        # Cambia al directorio principal del proyecto
+        os.chdir("/app/..")
+
+        # Imprime la lista de archivos en el directorio actual
+        ls_result = subprocess.run(["ls", "-l"], capture_output=True, text=True)
+
+        print(ls_result.stdout)
+        # Ejecuta el comando git checkout
+        subprocess.run(["git", "checkout", latest_release_name], check=True)
+
+        # Regresa al directorio original después de ejecutar el comando
+        os.chdir(current_directory)
+        message = f"Checkout exitoso a la última release ({latest_release_name})."
+
+    except subprocess.CalledProcessError as e:
+        message = f"Error al hacer checkout: {e}"
+else:
+    message = "No se pudo obtener la última release desde GitHub."
