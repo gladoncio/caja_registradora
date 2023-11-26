43e60fb4fe051510cf1542801f9e83e8b58d9179
gladoncio <42326122+gladoncio@users.noreply.github.com>
2023-11-26 16:09:55 -0300

    update test

--- update_latest_release.py
+++ update_latest_release.py
@@ -20,24 +20,32 @@ latest_release_name = check_github_version()
 if latest_release_name is not None:
     try:
         subprocess.run(["git", "fetch", "--all"])
-        
-        subprocess.run(["git", "reset", "--hard", f"origin/{latest_release_name}"])
 
-        subprocess.run(["git", "checkout", "-f", latest_release_name], check=True)
-
-        # Guarda la fecha, hora y versión en un archivo solo si el archivo no existe o está vacío
-        now = datetime.now()
-        timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+        # Obtener la versión almacenada en el archivo
         file_path = "update_info.txt"
-        
-        if not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
+        stored_version = None
+        if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
+            with open(file_path, "r") as file:
+                # Leer la última línea del archivo para obtener la versión almacenada
+                lines = file.readlines()
+                stored_version_line = lines[-1].strip()
+                stored_version = stored_version_line.split()[-1]
+
+        # Comprobar si la versión almacenada es la misma que la última versión de GitHub
+        if stored_version != latest_release_name:
+            subprocess.run(["git", "reset", "--hard", f"origin/{latest_release_name}"])
+            subprocess.run(["git", "checkout", "-f", latest_release_name], check=True)
+
+            # Guarda la fecha, hora y versión en un archivo
+            now = datetime.now()
+            timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
             with open(file_path, "a") as file:
                 file.write(f"{timestamp} - Versión actualizada a {latest_release_name}\n")
 
             message = f"Checkout exitoso a la última release ({latest_release_name})."
         else:
-            message = f"El archivo {file_path} ya existe y no está vacío. No se ha actualizado."
-            
+            message = f"La versión almacenada en el archivo ya es la última ({latest_release_name}). No se ha actualizado."
+
     except subprocess.CalledProcessError as e:
         message = f"Error al hacer checkout: {e}"
 else:
