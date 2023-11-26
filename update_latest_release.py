ab16e0c0688bb3408f0a0c29feeccec7986f3a14
gladoncio <42326122+gladoncio@users.noreply.github.com>
2023-11-26 15:21:10 -0300

    test 2

--- update_latest_release.py
+++ update_latest_release.py
@@ -6,7 +6,7 @@ import subprocess
 def check_github_version():
     url = "https://api.github.com/repos/gladoncio/caja_registradora/releases/latest"
     response = requests.get(url)
-    
+
     if response.status_code == 200:
         data = response.json()
         latest_version = data["tag_name"]
@@ -14,13 +14,13 @@ def check_github_version():
     else:
         # Manejar el error, puedes imprimir un mensaje o levantar una excepción según tus necesidades
         return None
-    
+
 latest_release_name = check_github_version()
 
 if latest_release_name is not None:
     try:
-        # Ejecuta el comando git checkout
-        subprocess.run(["git", "checkout", latest_release_name], check=True)
+        # Ejecuta el comando git checkout forzando (ignorando cambios locales)
+        subprocess.run(["git", "checkout", "-f", latest_release_name], check=True)
 
         # Guarda la fecha, hora y versión en un archivo
         now = datetime.now()
